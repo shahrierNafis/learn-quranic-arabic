917946fe4696294e923328159b17ec2911b87d83
shahrierNafis <102902752+shahrierNafis@users.noreply.github.com>
2026-05-19 16:01:35 +0600

    Refactor: clean up codebase and remove dead code

--- src/app/spacedRepetition/getFsrs.ts
+++
@@ -1,3 +0,0 @@
-import { useOnlineStorage } from "@/stores/onlineStorage";
-import { IPreview, Rating, RatingType, RecordLogItem, fsrs } from "ts-fsrs";
-export const preconfiguredFsrs = fsrs({ maximum_interval: useOnlineStorage.getState().maximumInterval });
